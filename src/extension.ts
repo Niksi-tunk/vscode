bd26e9d095125f2b027f5e48f3944d2c3a7876ab
Anton Podlozny <a.podlozny@gmail.com>
2024-03-07 12:48:28 +0200

    Add sidebar webview view

--- src/extension.ts
+++ src/extension.ts
@@ -19,6 +19,12 @@ export function activate(context: vscode.ExtensionContext) {
       }
     });
   }
+
+
+	const provider = new NiksiViewProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(NiksiViewProvider.viewType, provider));
 }
 
 function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
@@ -168,4 +174,41 @@ function getNonce() {
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
+}
+
+
+
+class NiksiViewProvider implements vscode.WebviewViewProvider {
+
+	public static readonly viewType = 'calicoColors.colorsView';  //se ei toimi muulla nimellä?? :D
+
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) {
+
+  }
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+	}
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+
+		return `
+          <html>
+          <body>
+              <button onclick="onClick()">Click me</button>
+          </body>
+          </html>
+      `;
+	}
+  
 }
