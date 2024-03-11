d3a2cd6b6fa5f6cd284b6403caacd6764cf8be7a
Matias Zwinger <matias.zwinger@protonmail.com>
2024-03-11 14:53:07 +0200

    fix: add missin comma

--- src/extension.ts
+++ src/extension.ts
@@ -7,7 +7,7 @@ export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.commands.registerCommand('niksi.new', () => {
       NiksiPanel.createOrShow(context.extensionUri);
-    })
+    }),
     vscode.commands.registerCommand('niksi.open', () => {
       NiksiPanel.createOrShow(context.extensionUri);
     })
